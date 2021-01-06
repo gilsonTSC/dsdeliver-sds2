4f1457485c7c43fc888681b7a50009270f25bb1c
GilsontTSC <gilson.costa@dcx.ufpb.br>
2021-01-06 18:39:15 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navebar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
