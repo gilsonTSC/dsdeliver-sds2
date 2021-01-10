804687730370a8e9884aa2298f79749b18d2f457
GilsontTSC <gilson.costa@dcx.ufpb.br>
2021-01-10 11:09:21 -0300

    Add orders list

---
+++ front-mobile/src/Routes.tsx
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Orders from "./Orders";
+import Home from "./Home";
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        headerMode="none"
+        screenOptions={{
+          cardStyle: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
